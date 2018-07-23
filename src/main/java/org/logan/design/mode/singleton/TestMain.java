b27b62e8989dc91709a94323e27898a833eb4f67
Logan <490211581@qq.com>
2018-07-23 22:47:27 +0800

    添加注释

--- src/main/java/org/logan/design/mode/singleton/TestMain.java
+++ src/main/java/org/logan/design/mode/singleton/TestMain.java
@@ -7,7 +7,10 @@ import java.io.ObjectOutputStream;
 import java.util.function.Consumer;
 
 /**
- * desc: 单例测试对象 <br/>
+ * desc: 测试单例模式 <br/>
+ * 单例的两大好处：<br/>
+ * 1，对于频繁使用的对象，可以省略创建对象消耗的时间，特别是重量级对象。<br/>
+ * 2，由于new操作次数减少，可以减轻GC压力，缩短GC停顿时间。<br/>
  * time: 2018/7/22 下午12:23 <br/>
  * author: Logan <br/>
  * since V 1.0 <br/>
